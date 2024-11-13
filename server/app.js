6cdb80ebce2b2abc77d2c04267b3c1fc9c5487c9
Vinayan AC <vinayanac7777@gmail.com>
2024-11-13 23:08:42 +0530

    vercel config added

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 import express from "express";
 import connect from "./config/connection.js";
 import userRoute from "./Routes/userRoute.js";
-import cors from "cors"
+import cors from "cors";
 const app = express();
 const PORT = 2003;
 connect();
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(
+  cors([
+    { origin: "http://localhost:5173" },
+    { origin: "https://todoapp0910.netlify.app/" },
+  ])
+);
 app.use("/api/user", userRoute);
 app.listen(PORT, () => console.log("OK"));
